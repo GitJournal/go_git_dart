09e73427b3feb35dcad093fec78630872d93ca46
Vishesh Handa <me@vhanda.in>
2023-10-20 14:46:43 +0200

    Use ffigen
    A little bit less boilerplate this way.

--- main.dart
+++ main.dart
@@ -1,25 +1,12 @@
 import 'dart:ffi' as ffi;
+import 'dart:ffi';
 import 'package:ffi/ffi.dart';
 import 'dart:io';
 
-typedef clone_func = ffi.Int32 Function(
-  ffi.Pointer<Utf8> url,
-  ffi.Pointer<Utf8> directory,
-  ffi.Pointer<ffi.Uint8> privateKeyBytes,
-  ffi.Int privateKeyLength,
-  ffi.Pointer<Utf8> passphrase,
-);
-typedef Clone = int Function(
-  ffi.Pointer<Utf8> url,
-  ffi.Pointer<Utf8> directory,
-  ffi.Pointer<ffi.Uint8> privateKeyBytes,
-  int privateKeyLength,
-  ffi.Pointer<Utf8> passphrase,
-);
+import 'generated_bindings.dart';
 
 void main() {
-  final dylib = ffi.DynamicLibrary.open(_getCorrectLibrary());
-  final Clone clone = dylib.lookupFunction<clone_func, Clone>('GitClone');
+  var lib = NativeLibrary(ffi.DynamicLibrary.open(_getCorrectLibrary()));
 
   final pemBytes = File('/Users/vishesh/.ssh/id_ed25519').readAsBytesSync();
 
@@ -32,13 +19,14 @@ void main() {
   var cloneDir = 'foo'.toNativeUtf8();
   var pemPassphrase = 'vishu002'.toNativeUtf8();
 
-  var retValue = clone(
-    cloneUrl,
-    cloneDir,
-    cPemBytes,
+  var retValue = lib.GitClone(
+    cloneUrl.cast<Char>(),
+    cloneDir.cast<Char>(),
+    cPemBytes.cast<Char>(),
     pemBytes.length,
-    pemPassphrase,
+    pemPassphrase.cast<Char>(),
   );
+
   print("ReturnValue: $retValue");
 
   malloc.free(cPemBytes);
