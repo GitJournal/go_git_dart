3b0783cebdffc329aefa875186196e64fb73c138
Vishesh Handa <me@vhanda.in>
2023-10-20 14:53:27 +0200

    Add a GitBindings class

--- main.dart
+++ main.dart
@@ -1,38 +1,59 @@
 import 'dart:ffi' as ffi;
 import 'dart:ffi';
+import 'dart:typed_data';
 import 'package:ffi/ffi.dart';
 import 'dart:io';
 
 import 'generated_bindings.dart';
 
-void main() {
-  var lib = NativeLibrary(ffi.DynamicLibrary.open(_getCorrectLibrary()));
+class GitBindings {
+  late final NativeLibrary lib;
 
-  final pemBytes = File('/Users/vishesh/.ssh/id_ed25519').readAsBytesSync();
-
-  var cPemBytes = malloc.allocate<ffi.Uint8>(pemBytes.length);
-  for (var i = 0; i < pemBytes.length; i++) {
-    cPemBytes[i] = pemBytes[i];
+  GitBindings(String libraryPath) {
+    lib = NativeLibrary(ffi.DynamicLibrary.open(libraryPath));
   }
 
-  var cloneUrl = 'git@github.com:vhanda/homelab'.toNativeUtf8();
-  var cloneDir = 'foo'.toNativeUtf8();
-  var pemPassphrase = 'vishu002'.toNativeUtf8();
+  void clone(
+    String url,
+    String directory,
+    Uint8List pemBytes,
+    String password,
+  ) {
+    var cPemBytes = malloc.allocate<ffi.Uint8>(pemBytes.length);
+    for (var i = 0; i < pemBytes.length; i++) {
+      cPemBytes[i] = pemBytes[i];
+    }
 
-  var retValue = lib.GitClone(
-    cloneUrl.cast<Char>(),
-    cloneDir.cast<Char>(),
-    cPemBytes.cast<Char>(),
-    pemBytes.length,
-    pemPassphrase.cast<Char>(),
-  );
+    var cloneUrl = url.toNativeUtf8();
+    var cloneDir = directory.toNativeUtf8();
+    var pemPassphrase = password.toNativeUtf8();
 
-  print("ReturnValue: $retValue");
+    var retValue = lib.GitClone(
+      cloneUrl.cast<Char>(),
+      cloneDir.cast<Char>(),
+      cPemBytes.cast<Char>(),
+      pemBytes.length,
+      pemPassphrase.cast<Char>(),
+    );
+    if (retValue != 0) {
+      throw Exception("GitClone failed with error code: $retValue");
+    }
 
-  malloc.free(cPemBytes);
-  malloc.free(cloneUrl);
-  malloc.free(cloneDir);
-  malloc.free(pemPassphrase);
+    malloc.free(cPemBytes);
+    malloc.free(cloneUrl);
+    malloc.free(cloneDir);
+    malloc.free(pemPassphrase);
+  }
+}
+
+void main() {
+  final pemBytes = File('/Users/vishesh/.ssh/id_ed25519').readAsBytesSync();
+  var cloneUrl = 'git@github.com:vhanda/homelab';
+  var cloneDir = 'foo';
+  var pemPassphrase = 'vishu002';
+
+  var bindings = GitBindings(_getCorrectLibrary());
+  bindings.clone(cloneUrl, cloneDir, pemBytes, pemPassphrase);
 }
 
 String _getCorrectLibrary() {
